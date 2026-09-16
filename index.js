01adfefe6a321dc9c6e9301a9bd9a9d3c478dd7d
rozkzzz <78845680+rozkzzz@users.noreply.github.com>
2026-09-16 16:34:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app  = express();
 const port = 3000;
 
+app.use(express.json());
 let tasks = [{
             id:1,
             title:'เรียน Express',
@@ -20,15 +21,61 @@ app.get('/tasks',function(req,res){
     res.json(tasks);
 });
 
+let nextTaskId = tasks.length+1;
+
+app.post('/tasks',function(req,res){
+    if(typeof req.body.title !== 'string' || (req.body.title).trim() ==='')
+        return res.status(400).json({message:'Title is required'});
+    let title_trim = (req.body.title).trim();
+    let newTask ={
+        id:nextTaskId,
+        title:title_trim,
+        completed:false,
+    }
+    nextTaskId++;
+    tasks.push(newTask);
+    return res.status(201).json(newTask);
+});
+
 app.get('/tasks/:id',function(req,res){
     let taskId = Number(req.params.id);
+    if (Number.isNaN(taskId))
+        return res.status(400).json({message:'ID must be a number'});
     let task = tasks.find(function(oneTask){
             return oneTask.id === taskId;
     });
-    res.json(task);
+    if(task === undefined)
+        return res.status(404).json({message:'Task not found'});
+    return res.json(task);
 });
 
+app.patch('/tasks/:id',function(req,res){
+    let taskId = Number(req.params.id);
+    if (Number.isNaN(taskId))
+        return res.status(400).json({message:'ID must be a number'});
+    let task = tasks.find(function(ele){
+        return ele.id === taskId;});
+    if (task === undefined)
+        return res.status(404).json({message:'cant find the id'});
+    if (typeof req.body.completed !== 'boolean')
+        return res.status(400).json({message:'Completed must be a boolean'});
+    task.completed = req.body.completed;
+    return res.json(task);
+});
 
+app.delete('/tasks/:id',function(req,res){
+    let taskId = Number(req.params.id);
+    if (Number.isNaN(taskId))
+        return res.status(400).json({message:'ID must be a number'});
+    let task = tasks.findIndex(function(element){
+        return taskId === element.id
+    });
+    if(task === -1)
+        return res.status(404).json({message:'Task not found'});
+    let deletedTask = tasks.splice(task,1)[0];
+    return res.json(deletedTask);
+
+});
 app.listen(port,function(){
     console.log('server is on');
 });
